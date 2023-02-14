49fa09e5e8e021aa6776ef69611eaa2cc57ba092
Kerol <102901360+Kerol-Dev@users.noreply.github.com>
2023-02-14 13:52:16 +0300

    Minor Changes
    A few comments are organized only.

--- src/main/java/frc/robot/Constants.java
+++ src/main/java/frc/robot/Constants.java
@@ -111,31 +111,16 @@ public final class Constants {
         public static final int pigeon_port = 0;
     }
 
-    /**
-     * Which PID slot to pull gains from. Starting 2018, you can choose from
-     * 0,1,2 or 3. Only the first two (0,1) are visible in web-based
-     * configuration.
-     */
     public static final int kSlotIdx = 0;
 
-    /**
-     * Talon FX supports multiple (cascaded) PID loops. For
-     * now we just want the primary one.
-     */
     public static final int kPIDLoopIdx = 0;
 
-    /**
-     * Set to zero to skip waiting for confirmation, set to nonzero to wait and
-     * report to DS if action fails.
-     */
     public static final int kTimeoutMs = 30;
 
     public static final class AutoConstants {
         public static final double kMaxSpeedMetersPerSecond = 3;
         public static final double kMaxAccelerationMetersPerSecondSquared = 1;
 
-        // Reasonable baseline values for a RAMSETE follower in units of meters and
-        // seconds
         public static final double kRamseteB = 2;
         public static final double kRamseteZeta = 0.7;
 
--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -1,7 +1,3 @@
-// Copyright (c) FIRST and other WPILib contributors.
-// Open Source Software; you can modify and/or share it under the terms of
-// the WPILib BSD license file in the root directory of this project.
-
 package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
@@ -47,28 +43,23 @@ public class Robot extends TimedRobot {
     }
   }
 
-  /** This function is called periodically during operator control. */
   @Override
   public void teleopPeriodic() {
   }
 
   @Override
   public void testInit() {
-    // Cancels all running commands at the start of test mode.
     CommandScheduler.getInstance().cancelAll();
   }
 
-  /** This function is called periodically during test mode. */
   @Override
   public void testPeriodic() {
   }
 
-  /** This function is called once when the robot is first started up. */
   @Override
   public void simulationInit() {
   }
 
-  /** This function is called periodically whilst in simulation. */
   @Override
   public void simulationPeriodic() {
   }
