582cf81eed279607a795b782e97bb94cac12dc73
Tamer Guven <83493008+Tamy0@users.noreply.github.com>
2023-03-05 22:14:10 +0300

    Added Leds
    Added ledsubsystem, methods to control the colour of the leds.

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -41,6 +41,8 @@ public class RobotContainer {
 
   public static SendableChooser<Integer> autoChooser = new SendableChooser<>();
 
+  public static boolean LedInterrupt = false;
+  
   public RobotContainer() {
     configureButtonBindings();
     autoChooser.setDefaultOption("TAXI", 0);
